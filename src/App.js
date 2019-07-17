fa43fcad5c56c7f93cf22b230a04752f4978664d
Virginia Thayer <virginia@stripe.com>
2019-07-17 13:48:56 -0700

    Card component created and rendering in DOM. Not yet set to handle video.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./Card"
+
+// RESPONSE
+// data:
+  // date: "2019-07-17"
+  // explanation: "(string)"
+  // media_type: "video"
+  // service_version: "v1"
+  // title: "Apollo 11: Descent to the Moon"
+  // url: "https://www.youtube.com/embed/xc1SzgGhMKc?start=850"
 
 function App() {
+
+  
+  
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Card />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// useEffect(() => {
+//   axios
+//     .get(`https://dog.ceo/api/breed/${breed}/images/random/15`)
+//     .then(response => {
+//       const doggos = response.data.message;
+//       // console.log("dogs api:", doggos);
+//       // KEY PART, SAVE TO STATE:
+//       setPets(doggos);
+//     });
+// }, [breed]);
--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './card.css';
+
+
+
+export default function Card() {
+    const [spacePic, setSpacePic] = useState({});
+    console.log('props passed to Card fn: ', spacePic)
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(response => {
+          // console.log('NASA APOD data: ', response)
+          setSpacePic(response.data);
+        })
+        .catch(error => {
+          console.log('Oops! ', error)
+        });
+      }, []);
+
+    return (
+        <div className = 'card'>
+            <img src={spacePic.url} alt={spacePic.title}/>
+        </div>
+    );
+}
+
