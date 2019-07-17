852287ac7dddccfeee5c2005f48c7dc6662a9f2c
Virginia Thayer <virginia@stripe.com>
2019-07-17 14:28:57 -0700

    Video workaround implemented and styling applied.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Card"
 
-// RESPONSE
-// data:
-  // date: "2019-07-17"
-  // explanation: "(string)"
-  // media_type: "video"
-  // service_version: "v1"
-  // title: "Apollo 11: Descent to the Moon"
-  // url: "https://www.youtube.com/embed/xc1SzgGhMKc?start=850"
 
 function App() {
 
-  
-  
-
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Space Photo of the Day!</h1>
       <Card />
+      <footer>
+      <p>Courtesy of <a href='https://www.nasa.gov/'>NASA</a></p>
+      </footer>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './card.css';
 
+// RESPONSE
+// data:
+  // date: "2019-07-17"
+  // explanation: "(string)"
+  // media_type: "video"
+  // service_version: "v1"
+  // title: "Apollo 11: Descent to the Moon"
+  // url: "https://www.youtube.com/embed/xc1SzgGhMKc?start=850"
 
 
 export default function Card() {
@@ -18,10 +25,21 @@ export default function Card() {
           console.log('Oops! ', error)
         });
       }, []);
-
+    if (spacePic.media_type === 'video') {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+        .then(response => {
+            setSpacePic(response.data)
+        })
+        .catch (error => {
+            console.log('Sorry, no space pic today. ', error)
+        })
+    }
+    if (!spacePic) return <h3>Loading...</h3>;
     return (
         <div className = 'card'>
             <img src={spacePic.url} alt={spacePic.title}/>
+            <h2>{spacePic.title}</h2>
+            <p>{spacePic.explanation}</p>
         </div>
     );
 }
